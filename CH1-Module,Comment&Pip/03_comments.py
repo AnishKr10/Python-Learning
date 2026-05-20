7a6391f57af041deae36700140d8dd5d03ccdf2d
AnishKr10 <anishmusic.management@gmail.com>
2026-05-20 19:27:12 +0530

    Single & Multi-line Comments

---
+++ CH1-Module,Comment&Pip/03_comments.py
@@ -0,0 +1,19 @@
+# This is a single line comment
+
+a = 5
+b = 10
+c = a + b
+print(c)
+
+# This is a Multi-line comment
+
+"""
+So Thanks 
+That was my program to add two numbers and print the result
+
+"""
+
+
+# This is multiple lines
+# here we are learning about comments in python
+# and how to use them in our code
